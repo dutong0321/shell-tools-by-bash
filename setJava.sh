9a05eb34e4c4d5ff1565101d70925a7e15af2b24
dutong <dutong0321@qq.com>
2018-02-05 09:57:38 +0800

    update setJava.sh

--- setJava.sh
+++ setJava.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
+echo 'this tool must need sudo right'
 echo 'Please input JAVA_HOME:'
 read JAVA_HOME_PATH
 echo 'export JAVA_HOME='$JAVA_HOME_PATH >> /etc/profile
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
 echo 'export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar' >> /etc/profile
 source /etc/profile
+echo '如果配置失败，重启后将会自动配置成功'
+echo 'if set Java enviroment fail,please try to restart,then will be success'
+
